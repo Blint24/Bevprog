a75415ca7b1ddabdf720630e4d1442aa1dbdfe48
Blint24 <90709616+Blint24@users.noreply.github.com>
2021-10-08 22:59:37 +0200

    Add files via upload

---
+++ drill4.1.cpp
@@ -0,0 +1,45 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+	constexpr double mtocm = 0.01;
+	constexpr double intocm = 2.54;
+	constexpr double fttoin = 12;
+	double min = 0;
+	double max = 0;
+	double a;
+	vector<double> szamok;
+	double sum = 0;
+	string unit;
+	while(cin >> a >> unit){	
+			if (unit == "m"){
+				szamok.push_back(a);
+				sum += a;
+			}else if (unit == "in"){
+				szamok.push_back(a * intocm * mtocm);
+				sum += a * intocm * mtocm;
+			}else if (unit == "ft"){
+				szamok.push_back(a * fttoin * intocm * mtocm);
+				sum += a * fttoin * intocm * mtocm;
+			}else if (unit == "cm"){
+				szamok.push_back(a * mtocm);
+				sum += a * mtocm;
+			}else{
+				cout << "Value rejected\n";
+			}			
+			if((a < min) || (min == 0)){
+				min = a;
+				cout << "smallest number so far: " << min << "\n";
+			}else if((a > max) || (max == 0)){
+				max = a;
+				cout << "largest number so far: " << max << "\n";			
+			}
+	}
+	cout << "Smallest number: " << min << "\n";
+	cout << "Largest number: " << max << "\n";
+	cout << "Number of values entered: " << szamok.size() << "\n";
+	cout << "Sum of values: " << sum << "\n";
+	sort(szamok);
+	for (double values : szamok) cout << values << "\n";
+	return 0;
+}
